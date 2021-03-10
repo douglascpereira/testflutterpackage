dc4c04e2b7c1e5c3ee35192d04fc20e903205c07
Douglas <douglascpereira@gmail.com>
2021-03-10 11:37:23 -0300

    ajuste package

--- lib/testflutterpackage.dart
+++ lib/testflutterpackage.dart
@@ -4,16 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class CallURL extends StatelessWidget {
-  const CallURL({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: callURL(context),
-    );
-  }
-
+class CallURL {
   Future<void> _launch() async {
     final String urlString = "https://www.google.com.br";
     if (await canLaunch(urlString)) {
